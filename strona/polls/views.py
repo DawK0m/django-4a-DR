2ed0ee780e721c1cebec6826791c9705f62c0340
DawK0m <dawidromanis@gmail.com>
2024-03-01 08:38:59 +0100

    models

--- strona/polls/views.py
+++ strona/polls/views.py
@@ -2,4 +2,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("hello world")
+
+def counter(request):
+    text_file = open("./templates/counter.html", "r")
+    page = text_file.read()
+    text_file.close()
+    return HttpResponse(page)
+
+def basic(request):
+    plik = open("./templates/basic.html", "r")
+    strona = plik.read()
+    plik.close()
+    return HttpResponse(strona)
