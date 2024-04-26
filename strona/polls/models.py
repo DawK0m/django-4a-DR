7d359031524acc04087d250dddbf59df2f4a810b
DawK0m <dawidromanis@gmail.com>
2024-04-26 07:34:06 +0200

    dawijdwiada

--- strona/polls/models.py
+++ strona/polls/models.py
@@ -12,6 +12,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -19,8 +21,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.question_text
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
